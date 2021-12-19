0f12b7621ddeb1f9b17155d61879534c4d737f82
JOJOVAV <jojovav.jvav@gmail.com>
2021-12-19 17:34:01 +0100

    upload code and there is little fault in the code

---
+++ r6Launcher/Forms/FormAddSiege.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace r6Launcher.Forms
+{
+    public partial class FormAddSiege : Form
+    {
+        
+
+        
+        public FormAddSiege(int value)
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void FormAddSiege_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void comboBoxAddSiege_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*public sealed class FolderBrowserDialog : System.Windows.Forms.CommonDialog
+        {
+            private FolderBrowserDialog folderBrowserDialog1;
+            private OpenFileDialog openFileDialog1;
+        }*/
+    }
+}
---
+++ r6Launcher/Forms/FormSelectSiege.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace r6Launcher.Forms
+{
+    public partial class FormSelectSiege : Form
+    {
+        /*System.Windows.Forms.Form f = System.Windows.Forms.Application.OpenForms["Form1"];
+        System.Windows.Forms.Form formAdd = System.Windows.Forms.Application.OpenForms["FormAddSiege"];
+        System.Windows.Forms.Form formSelect = System.Windows.Forms.Application.OpenForms["FormSelectSiege"];*/
+        public FormSelectSiege()
+        {
+            InitializeComponent();
+        }
+    }
+}
